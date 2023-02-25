4eea51fab61658e545875121bed9a4c1275f885e
PSNYAutomation <psny.automation@gmail.com>
2023-02-25 06:52:27 -0500

    Added appropriate comments in already added class in previous commit

--- AutomationFeb23/src/variables/CalculatorTest.java
+++ AutomationFeb23/src/variables/CalculatorTest.java
@@ -4,7 +4,9 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		
+		//Creating a reference variable of Calculator class
 		Calculator calc = new Calculator();
+		//Testing div method 
 		System.out.println(calc.div(75, 3)); 
 
 	}
